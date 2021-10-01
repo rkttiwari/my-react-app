7bb69f6f83d9845f3e06ca551d987da5d632126d
Rakesh Tiwari <rt@amadeus.co.in>
2021-10-01 16:32:03 +0530

    Created expense item component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './header';
-import Footer from './footer';
-import Home from './home';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./header";
+import Footer from "./footer";
+import Home from "./home";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: "12.67",
+      date: new Date(2021, 1, 10),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: "294.67",
+      date: new Date(2021, 1, 10),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: "212.67",
+      date: new Date(2021, 1, 10),
+    },
+    {
+      id: "e4",
+      title: "New Desk",
+      amount: "312.67",
+      date: new Date(2021, 1, 10),
+    },
+  ];
+
   return (
-    <div className="App">
-    <Header />
-    
-    <div class="container">
-      <div class="row">
-        <Home /><Home /><Home />
-      </div>
+    <div>
+      <h2>Let's Get Started</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+
+      {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem> */}
     </div>
-    
-    <Footer />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+
+    // <div className="App">
+    // <Header />
+
+    // <div class="container">
+    //   <div class="row">
+    //     <Home /><Home /><Home />
+    //   </div>
+    // </div>
+
+    // <Footer />
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
   );
 }
 
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+function ExpenseItem (props) {
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h3>{props.title}</h3>
+                <div className='expense-item__price'>$ {props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
